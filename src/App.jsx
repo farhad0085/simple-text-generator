a4da331e79502ab98eb4ef4fd34c1515bf55f4f0
Farhad Hossain <farhadhossain0085@gmail.com>
2020-11-17 04:24:28 +0600

    redux implemented and loading added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './app.css'
 const App = props => {
     return (
         <div className="container">
-            <h1>ReactJS Sample text generator</h1>
+            <h1 className="display-4">ReactJS Sample text generator</h1>
             <hr />
             <Controller />
             <Output value={props.text} />
